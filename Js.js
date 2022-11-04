1fe9ad8324f7e4f8334b4637ca27f2afc471c191
Sophie Kua <sophie.kua.2021@rhul.ac.uk>
2022-11-04 22:48:01 +0000

    adding background change to buttons

--- Js.js
+++ Js.js
@@ -1,1 +1,13 @@
 // JavaScript source code
+function spring(){
+    document.getElementById("homeBody").style.backgroundImage = "url(cherry-blossom-falling.gif)";
+}
+function summer(){
+    document.getElementById("homeBody").style.backgroundImage = "url(summer.gif)";
+}
+function autumn(){
+    document.getElementById("homeBody").style.backgroundImage = "url(autumn.gif)";
+}
+function winter(){
+    document.getElementById("homeBody").style.backgroundImage = "url(winter.gif)";
+}
